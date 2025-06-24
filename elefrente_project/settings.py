1540201a7204d5d7654bdb16a83c1c873fa20690
Sanjay Ramchandani <sanjay@hotmail.es>
2025-06-24 18:05:59 -0400

    add env

--- elefrente_project/settings.py
+++ elefrente_project/settings.py
@@ -13,9 +13,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Cargar variables desde .env si existe
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
