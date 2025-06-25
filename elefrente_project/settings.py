953072e8ca5b11e4dac7b07b98b25e1f7bfcc5ac
Sanjay Ramchandani <sanjay@hotmail.es>
2025-06-25 14:10:33 -0400

    redirccion

--- elefrente_project/settings.py
+++ elefrente_project/settings.py
@@ -152,3 +152,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirecciones tras autenticación
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
